bb8248b82ac0a523db26a6e44b7d9324e7939f6f
kunalpan <panchalkunal9498@gmail.com>
2021-03-12 16:09:54 +0530

    master files added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const createConnection = require('./dbUtils/dbConnection');
 
 (async () => {
 	await createConnection(app);
 	/* Middleware */
+	app.use(cors());
 	app.use('/', require('./routers/router'));
 })();
